e0f0b52215904ce344b2f5fbaac7df4c718d4020
Victor-P-V <pavloff.vik2018@gmail.com>
2024-11-16 23:13:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print ("Hello world")
+# "1st program".
+print(9**0.5*5)
+# "2nd program".
+print(9.99>9.98 or 1000!=1000.1)
+# "3rd program"
+print(2*2+2==2*(2+2))
+x = float('123.456')
+print(int((x*10)%10))
